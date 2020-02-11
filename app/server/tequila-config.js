3716954d1557389374c5e64b39e0a6c5f67988a1
Grégoy Charmier <gregory.charmier@gmail.com>
2020-02-11 19:09:49 +0100

    Fix default role

--- app/server/tequila-config.js
+++ app/server/tequila-config.js
@@ -9,13 +9,13 @@ Tequila.start({
     if (groups.includes('wp-veritas-admins')) {
       Roles.setUserRoles(tequila.uniqueid, ['admin'], Roles.GLOBAL_GROUP);
     } else if (groups.includes('wp-veritas-editors')) {
-      Roles.setUserRoles(tequila.uniqueid, ['epfl-member'], Roles.GLOBAL_GROUP);
-    } else {
       Roles.setUserRoles(tequila.uniqueid, ['editor'], Roles.GLOBAL_GROUP);
+    } else {
+      Roles.setUserRoles(tequila.uniqueid, ['epfl-member'], Roles.GLOBAL_GROUP);
     }
     // Greg is admin forever and even after
     if (tequila.uniqueid == "188475") {
-      // Roles.setUserRoles(tequila.uniqueid, ['admin'], Roles.GLOBAL_GROUP); 
+      Roles.setUserRoles(tequila.uniqueid, ['admin'], Roles.GLOBAL_GROUP); 
     }
     return tequila.uniqueid;
   },
