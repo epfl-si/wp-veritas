0064e9b6e67a9ecb39a7c53d6b3a82dbb3cca459
Grégory Charmier <gregory.charmier@gmail.com>
2020-03-19 19:37:45 +0100

    Import all test sites

--- app/server/import-data.js
+++ app/server/import-data.js
@@ -1,3 +1,73 @@
-importData = () => {}
+import { sitesSchema } from '../imports/api/schemas/sitesSchema';
+import { Sites } from '../imports/api/collections';
+
+importData = () => {
+
+  Sites.remove({});
+
+  var myjson = {};
+  myjson = JSON.parse(Assets.getText("inventory-test.json"));
+  let sites = myjson['_meta']['hostvars'];
+  // console.log(sites);
+
+  for (var currentSite in sites) {
+    let site = myjson['_meta']['hostvars'][currentSite];
+
+    if (site.wp_hostname !== 'migration-wp.epfl.ch') {
+      continue;
+    }
+    if (site.wp_env !== 'int') {
+      continue;
+    }
+    console.log(site);
+    
+    let url = `https://${ site.wp_hostname }/${ site.wp_path }`;
+    let title = site.wp_path;
+    let category = site['wp_details']['options']['epfl:site_category'];
+    if (category == null) {
+      category = 'GeneralPublic';
+    }
+    let theme = site['wp_details']['options']['stylesheet'];;
+    let languages = site['wp_details']['polylang']['langs'];
+    if (!languages) {
+      continue;
+    }
+    let unitId = site['wp_details']['options']['plugin:epfl_accred:unit_id'];
+    let unitName = site['wp_details']['options']['plugin:epfl_accred:unit'];
+
+    console.log(url);
+    console.log(title);
+    console.log(category);
+    console.log(theme);
+    console.log(languages);
+    console.log(unitId);
+    console.log(unitName);
+
+    let siteDocument = {
+      url: url,
+      tagline: '',
+      title: title,
+      wpInfra: true,
+      openshiftEnv: 'int',
+      category: category,
+      theme: theme,
+      languages: languages,
+      unitId: unitId,
+      unitName: unitName,
+      unitNameLevel2: '',
+      snowNumber: '',
+      comment: '',
+      userExperience: false,
+      slug: '',
+      professors: [],
+      tags: [],
+    }
+    
+    sitesSchema.validate(siteDocument);
+
+    let newSiteId = Sites.insert(siteDocument);
+    
+  }
+}
 
 export { importData }
