87c36279cb6d8f10541698e02d3f25f8450896d9
Grégory Charmier <gregory.charmier@gmail.com>
2020-04-17 09:53:20 +0200

    Delete UpdateRole => now groups.epfl.ch define Admin or Editor user role

--- app/imports/api/methods.js
+++ app/imports/api/methods.js
@@ -140,32 +140,6 @@ Meteor.methods({
     });
   },
 
-  updateRole(userId, role) {
-      
-    if (!this.userId) {
-      throw new Meteor.Error('not connected');
-    }
-    const canUpdate = Roles.userIsInRole(
-      this.userId,
-      ['admin'], 
-      Roles.GLOBAL_GROUP
-    );
-    if (! canUpdate) {
-      throw new Meteor.Error('unauthorized',
-        'Only admins can update roles.');
-    }
-    
-    let roleBeforeUpdate = Roles.getRolesForUser(userId);
-
-    Roles.setUserRoles(userId, [role], Roles.GLOBAL_GROUP);
-
-    AppLogger.getLog().info(
-      `Update role ID ${ userId }`, 
-      { before: roleBeforeUpdate, after: [role] },
-      userId
-    );
-  },
-
   insertTag(tag){
         
     if (!this.userId) {
