83fd28ee7535a9e583127a282269ceaff1f5791c
Grégory Charmier <gregory.charmier@gmail.com>
2020-04-22 10:01:26 +0200

    Impossible to use ValidatedMethod with async/await meteor methods ?

--- app/imports/api/methods.js
+++ app/imports/api/methods.js
@@ -1,7 +1,7 @@
-import { Sites } from "../api/collections";
-import { AppLogger } from "../api/logger";
-
+// TODO: How use ValidatedMethod with async/await meteor methods ?
+// I have the same problem as https://stackoverflow.com/questions/54401422/meteor-async-validatedmethod-gets-called-with-function-parameters-undefined
 Meteor.methods({
+
   async getUserFromLDAP(sciper) {
     let result;
     const publicLdapContext = require("epfl-ldap")();
