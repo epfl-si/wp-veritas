bd35f5e03fc2ec3c9b21e21146bbea39c0c34db7
Nicolas Borboën <ponsfrilus@gmail.com>
2020-06-10 14:44:18 +0200

    [feature] Add github sources in the footer

--- app/imports/ui/components/footer/Footer.jsx
+++ app/imports/ui/components/footer/Footer.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export const Footer = () => (
   <footer className="small border-top">
-      <div className="footer-copyright text-center py-3">© 2019 Copyright 
-          <a href="https://www.epfl.ch"> EPFL</a>
+      <div className="footer-copyright text-center py-3">&copy; 2019 Copyright 
+          &nbsp;<a href="https://www.epfl.ch">EPFL</a>
+          &nbsp;&mdash;&nbsp;<a href="https://github.com/epfl-si/wp-veritas">sources</a>
       </div>
   </footer>
 )
