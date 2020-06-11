1f5ac7baab39ccd74bc7e6a259a78538e870594a
Grégory Charmier <gregory.charmier@gmail.com>
2020-06-11 09:00:05 +0200

    Format with Prettier

--- app/imports/api/error.js
+++ app/imports/api/error.js
@@ -1,7 +1,6 @@
-
 export function throwMeteorError(fieldName, message) {
-    const ddpError = new Meteor.Error(message);
-    ddpError.error = 'validation-error';
-    ddpError.details = [{name: fieldName, message: message}];
-    throw ddpError;
+  const ddpError = new Meteor.Error(message);
+  ddpError.error = "validation-error";
+  ddpError.details = [{ name: fieldName, message: message }];
+  throw ddpError;
 }
