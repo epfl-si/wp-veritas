d43f9d724901d2f8fcd51cfa2abf717ef9d8f6e1
Grégory Charmier <gregory.charmier@gmail.com>
2020-06-17 18:57:58 +0200

    [workflow] Disable import data

--- app/server/main.js
+++ app/server/main.js
@@ -16,7 +16,7 @@ import { importData } from "./import-data";
 import { AppLogger } from "../imports/api/logger";
 import "./indexes";
 
-let importDatas = true;
+let importDatas = false;
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
