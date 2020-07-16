9f8c782e3b4322415e3bdec42ba212ecbfdaed6e
Grégory Charmier <gregory.charmier@gmail.com>
2020-07-16 16:39:30 +0200

    [version] Add a new version number

--- app/imports/ui/components/header/Header.jsx
+++ app/imports/ui/components/header/Header.jsx
@@ -149,7 +149,7 @@ class Header extends Component {
                     >
                       Voir les logs
                     </NavLink>
-                    <div className="dropdown-item">Version 1.7.6</div>
+                    <div className="dropdown-item">Version 1.7.9</div>
                   </div>
                 </li>
               ) : null}
