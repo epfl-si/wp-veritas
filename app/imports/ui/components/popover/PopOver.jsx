acfe89d24d7270f464cab93068a2dd1515bbc934
Nicolas Borboën <ponsfrilus@gmail.com>
2020-07-29 12:44:27 +0200

    [fix] svg icon from the public url
    Should fix #94

--- app/imports/ui/components/popover/PopOver.jsx
+++ app/imports/ui/components/popover/PopOver.jsx
@@ -1,14 +1,10 @@
 /**
  * Display a information popover using popper.js
  * See https://reactstrap.github.io/components/popovers/
- * 
+ *
  */
-import React, { useState } from 'react';
-import { Popover, PopoverHeader, PopoverBody } from 'reactstrap';
-
-// TODO: This SVG not works in test infra. Convert SVG to correct JSX ?
-//import infoImg from './icons8info.svg';
-import infoImg from '../header/Logo_EPFL.svg';
+import React, { useState } from "react";
+import { Popover, PopoverHeader, PopoverBody } from "reactstrap";
 
 const PopOver = (props) => {
   const [popoverOpen, setPopoverOpen] = useState(false);
@@ -16,13 +12,23 @@ const PopOver = (props) => {
 
   return (
     <span className="float-md-right">
-      <img src={infoImg} width="20px" id="Popover1" type="button" />
-      <Popover placement={props.placement ? props.placement : 'right'} isOpen={popoverOpen} target="Popover1" toggle={toggle}>
+      <img
+        src="/img/icons/Infobox_info_icon.svg"
+        width="20px"
+        id="Popover1"
+        type="button"
+      />
+      <Popover
+        placement={props.placement ? props.placement : "right"}
+        isOpen={popoverOpen}
+        target="Popover1"
+        toggle={toggle}
+      >
         <PopoverHeader>{props.title}</PopoverHeader>
         <PopoverBody>{props.description}</PopoverBody>
       </Popover>
-      </span>
+    </span>
   );
-}
+};
 
 export default PopOver;
