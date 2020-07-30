2d9010ba92c525861e004cdcf5202d730f160223
Nicolas Borboën <ponsfrilus@gmail.com>
2020-07-30 17:17:46 +0200

    [update] config sample

--- cli/lib/config.sample.js
+++ cli/lib/config.sample.js
@@ -1,16 +1,20 @@
-// Connection string: mongodb://username:password@host:port/database
-
+// Connection string: mongodb://username:password@host/database
 
 // Sources
 exports.PROD_DB_HOST = "XXX";
 exports.PROD_DB_USERNAME = "wp-veritas";
-exports.PROD_DB_PWD = "XXX";
+exports.PROD_DB_PWD = process.env.WP_VERITAS_DB_PASSWORD_PROD;
 exports.PROD_DB_NAME = "wp-veritas";
 
 exports.TEST_DB_HOST = "XXX";
-exports.TEST_DB_USERNAME = "wp-veritas";
-exports.TEST_DB_PWD = "XXX";
-exports.TEST_DB_NAME = "wp-veritas";
+exports.TEST_DB_USERNAME = "wp-veritas-test";
+exports.TEST_DB_PWD = process.env.WP_VERITAS_DB_PASSWORD_TEST;
+exports.TEST_DB_NAME = "wp-veritas-test";
+
+exports.DEV_DB_HOST = "XXX";
+exports.DEV_DB_USERNAME = "wp-veritas";
+exports.DEV_DB_PWD = process.env.WP_VERITAS_DB_PASSWORD_DEV;
+exports.DEV_DB_NAME = "wp-veritas";
 
 // Local target (configurable in case you are using another installation of
 // mongodb, e.g. inside a docker container)
