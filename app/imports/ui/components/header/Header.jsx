2969e996f94e2405df56943b10a1a8683553195e
Grégory Charmier <gregory.charmier@gmail.com>
2021-03-18 10:51:01 +0100

    [version] 1.12.4

--- app/imports/ui/components/header/Header.jsx
+++ app/imports/ui/components/header/Header.jsx
@@ -157,7 +157,7 @@ class Header extends Component {
                     >
                       Voir les logs
                     </NavLink>
-                    <div className="dropdown-item">Version 1.12.3</div>
+                    <div className="dropdown-item">Version 1.12.4</div>
                   </div>
                 </li>
               ) : null}
