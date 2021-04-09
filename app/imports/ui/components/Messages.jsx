dfaccb98b022052adb038790614cac86fc89fae9
Grégory Charmier <gregory.charmier@gmail.com>
2021-04-09 10:21:03 +0200

    [feature] Displays a message to the user when the AWX job has started and when it has finished

--- app/imports/ui/components/Messages.jsx
+++ app/imports/ui/components/Messages.jsx
@@ -7,6 +7,12 @@ export const AlertSuccess = (props) => (
   </div>
 );
 
+export const AlertDanger = (props) => (
+  <div className="alert alert-danger" role="alert">
+    {props.message}
+  </div>
+);
+
 export const AlertSiteSuccess = (props) => (
   <div className="alert alert-success" role="alert">
     Le nouveau site <a href={"/edit/" + props.id}>{props.title}</a> a été ajouté avec succès !
