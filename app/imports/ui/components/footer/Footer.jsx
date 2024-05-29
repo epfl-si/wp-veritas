cb21e82013814025b8c88f848d86ad23e5c8f66d
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-05-29 16:51:36 +0200

    [chore] Update copyright footer

--- app/imports/ui/components/footer/Footer.jsx
+++ app/imports/ui/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () => (
   <footer className="small border-top">
-      <div className="footer-copyright text-center py-3">&copy; 2019 Copyright 
+      <div className="footer-copyright text-center py-3">&copy; 2024 Copyright 
           &nbsp;<a href="https://www.epfl.ch">EPFL</a>
           &nbsp;&mdash;&nbsp;<a href="https://github.com/epfl-si/wp-veritas">sources</a>
       </div>
