8f13175ede1b2b8203267ec12bce31ebf47bad25
Azecko <samikarin24@gmail.com>
2025-04-17 15:25:49 +0200

    [WIP] Upsert

--- app/server/sites.js
+++ app/server/sites.js
@@ -1,23 +1,28 @@
 import { watchWPSites } from "./kubernetes"
 import { Sites } from "../imports/api/collections"
 
-Meteor.startup( () => {
+Meteor.startup( async () => {
   watchWPSites({added, removed});
 
   async function added(site) {
-    await Sites.insertAsync({
-      k8sName: site.metadata.name,
-      url: site.spec.hostname + site.spec.path,
-      title: site.spec.wordpress.title,
-      tagline: site.spec.wordpress.tagline,
-      openshiftEnv: "kubernetes",
-      categories: [],
-      theme: site.spec.wordpress.theme,
-      platformTarget: "kubernetes",
-      languages: site.spec.wordpress.languages,
-      unitId: site.spec.owner.epfl.unitId,
-      createdDate: site.metadata.creationTimestamp,
-    });
+    const url = site.spec.hostname + site.spec.path;
+    await Sites.upsertAsync(
+      { url },
+      {
+        $set: {
+          k8sName: site.metadata.name,
+          title: site.spec.wordpress.title,
+          tagline: site.spec.wordpress.tagline,
+          openshiftEnv: "kubernetes",
+          categories: [],
+          theme: site.spec.wordpress.theme,
+          platformTarget: "kubernetes",
+          languages: site.spec.wordpress.languages,
+          unitId: site.spec.owner.epfl.unitId,
+          createdDate: site.metadata.creationTimestamp,
+        }
+      }
+    );
   }
 
   async function removed(site) {
@@ -29,4 +34,38 @@ Meteor.startup( () => {
       throw new Error(`Unexpected multiple matches on a search on ${site.metadata.name}: ${toDelete.length} items`)
     }
   }
+
+  const MongoSites = new Mongo.Collection('sites');
+  const cursor = MongoSites.find({});
+  for (const site of await cursor.fetchAsync()) {
+    delete site._id;
+    await Sites.upsertAsync(
+      { url: site.url },
+      {
+        $set: site
+      }
+    );
+  }
+  cursor.observeChangesAsync({
+    async added(id, fields) {
+      delete fields._id;
+      await Sites.upsertAsync(
+        { url: fields.url },
+        {
+          $set: {...fields, _id_mongo: id}
+        }
+      );
+    },
+    async changed(id, fields) {
+      await Sites.updateAsync(
+        { _id_mongo: id },
+        {
+          $set: fields
+        }
+      );
+    },
+    // removed(id) {
+    //   ...
+    // }
+  })
 });
