6dafaf2227d1ff05001f9545a5d5f6539a2c1bf7
Nicolas Borboën <ponsfrilus@gmail.com>
2025-04-28 15:45:03 +0200

    [refactor] Footer

--- app/imports/ui/components/footer/Footer.jsx
+++ app/imports/ui/components/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+var pjson = require('/package.json');
 
 export const Footer = () => (
   <footer className="small border-top">
-      <div className="footer-copyright text-center py-3">&copy; 2024 Copyright 
-          &nbsp;<a href="https://www.epfl.ch">EPFL</a>
+      <div className="footer-copyright text-center py-3">Coded by
+          &nbsp;<a href="https://go.epfl.ch/fsd">ISAS-FSD</a>
           &nbsp;&mdash;&nbsp;<a href="https://github.com/epfl-si/wp-veritas">sources</a>
+          &nbsp;&mdash;&nbsp;v{pjson.version}
       </div>
   </footer>
 )
