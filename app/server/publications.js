8a7784050ce9ad5017ddc4b3e6d75618df41aa34
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-04-28 15:34:49 +0200

    [rewrite] publication (Kubernetes + Mongo) merging logic
    - Revert to an “ordinary”, persistent serialization for “local” sites
    - Merge at publication time using Meteor's “mergebox”, documented at
    https://guide.meteor.com/data-loading#lifecycle (TL;DR: publish twice
    “into” the same client-side MiniMongo)
    - Eliminate the non-persistent, server-side MiniMongo

--- app/server/publications.js
+++ app/server/publications.js
@@ -8,32 +8,87 @@ import {
 } from "../imports/api/collections";
 import { check } from "meteor/check";
 import { Roles } from "meteor/alanning:roles";
+import { watchWPSites } from "./kubernetes";
 import Debug from "debug";
 
 const debug = Debug("server/publications");
 
 Meteor.publish("sites.list", async function () {
-  debug("sites.list: subscribed");
-  const cursor = Sites.find({});
+  const cursor = Sites.find({})
+  const urlOfMongoId = {}
+
+  // In order to let Meteor's so-called “mergebox” do its job
+  // w.r.t. the next Meteor.publish down below
+  // (as per https://guide.meteor.com/data-loading#lifecycle),
+  // we can't just return the cursor; we need to remap
+  // the sites' URL as the client-side primary key.
+
+  const added = this.added.bind(this, "sites"),
+        changed = this.changed.bind(this, "sites"),
+        removed = this.removed.bind(this, "sites")
   for (const site of await cursor.fetchAsync()) {
-    this.added("sites", site._id, site);
-    // TODO: turn into a live query
+    urlOfMongoId[site._id] = site.url
+    added(site.url, site)
   }
-
-  this.ready();
-
-  const that = this;
+  this.ready()
   cursor.observeChangesAsync({
     added(id, fields) {
-      that.added("sites", id, fields);
+      const site = fields
+      urlOfMongoId[id] = site.url
+      added(site.url, site)
     },
     changed(id, fields) {
-      that.changed("sites", id, fields);
+      const url = urlOfMongoId[id]
+      if (fields.url) {
+        throw new Error("Cannot mutate url")
+      }
+      changed(url, fields)
     },
     removed(id) {
-      that.removed("sites", id);
+      const url = urlOfMongoId[id]
+      removed(url)
     }
+  }).then((observer) => {
+    this.onStop(() => observer.stop())
   })
+});
+
+Meteor.publish("k8ssites.list", function () {
+  watchWPSites({added, removed});
+  this.ready();
+
+  const add = this.added.bind(this, "sites"),
+        change = this.changed.bind(this, "sites"),
+        remove = this.removed.bind(this, "sites")
+
+  function getUrl (site) {
+    return `https://${site.spec.hostname}${site.spec.path}${!site.spec.path.endsWith("/") ? "/" : ""}`;
+  }
+
+  async function added (site) {
+    const url = getUrl(site)
+    debug(`Added from k8s: ${url}`)
+    add(url,
+      {
+          url,
+          k8sName: site.metadata.name,
+          title: site.spec.wordpress.title,
+          tagline: site.spec.wordpress.tagline,
+          type: site.spec.type,
+          categories: [],
+          theme: site.spec.wordpress.theme,
+          languages: site.spec.wordpress.languages,
+          unitId: site.spec.owner.epfl.unitId,
+          createdDate: site.metadata.creationTimestamp,
+      }
+    );
+  }
+
+  async function removed (site) {
+    const url = getUrl(site)
+    debug(`Removed from k8s: ${url}`)
+    remove(url)
+  }
 });
 
 Meteor.publish("deleteSites.list", async function () {
