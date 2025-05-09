ffca367c683791d5d33d74582639d2a81e912752
Nicolas Borboën <ponsfrilus@gmail.com>
2025-05-09 15:56:24 +0200

    [feature] Add a identifier in site's title when not in prod
    Need a way to identify localhost and test from the page's title.

--- app/client/main.js
+++ app/client/main.js
@@ -4,9 +4,12 @@ import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
 import { Sites, Themes } from "../imports/api/collections";
 import App from "../imports/ui/App";
+import { getEnvironment } from "../imports/api/utils";
 
 Meteor.startup(() => {
   render(<App />, document.getElementById("render-target"));
+  document.title = getEnvironment() == 'PROD' ? '' :
+                       (getEnvironment() == 'LOCALHOST' ? '♘ WP-Veritas' : '♖ WP-Veritas');
   Tequila.start();
 });
 
