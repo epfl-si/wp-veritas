ac48fd3ea12d3146be6b83793ae3f38b99203d8c
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-04 15:44:37 +0200

    [feature] Welcome to Next.js

--- app/imports/api/index.js
+++
@@ -1,2 +0,0 @@
-export * from '../api/collections';
-import '../api/methods';
--- app/imports/api/schemas/siteExternal.js
+++
@@ -1,6 +0,0 @@
-import SimpleSchema from 'simpl-schema';
-import { siteBase } from './siteBaseSchema';
-
-export const siteExternalSchema = new SimpleSchema({
-  ...siteBase
-}, { check });
