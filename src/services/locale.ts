ca62738616f8da83c025804c357fecbfef61a6d2
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-05 09:16:44 +0200

    [feature] implement user locale management with cookie support

---
+++ src/services/locale.ts
@@ -0,0 +1,16 @@
+'use server';
+
+import { defaultLocale, Locale } from '@/i18n/config';
+import { cookies } from 'next/headers';
+
+const COOKIE_NAME = 'locale';
+
+export async function getUserLocale() {
+	const userCookies = await cookies();
+	return userCookies.get(COOKIE_NAME)?.value || defaultLocale;
+}
+
+export async function setUserLocale(locale: Locale) {
+	const userCookies = await cookies();
+	userCookies.set(COOKIE_NAME, locale);
+}
