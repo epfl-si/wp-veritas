d5fefb54b7590a3dc92aaaf55a46fff63a34b3d1
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-05 09:18:20 +0200

    [feature] add RootLayout component with user authentication and layout structure

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+'use server';
+import { Fragment, ReactNode } from 'react';
+import React from 'react';
+import { Header } from '@/components/header';
+import { getUser } from '@/services/auth';
+import { Sidebar } from '@/components/sidebar';
+
+export default async function RootLayout({ children }: { children: ReactNode }) {
+	const user = await getUser();
+
+	return (
+		<Fragment>
+			<Header user={user} />
+			<main className="flex sm:h-[calc(100%-90px)]">
+				<Sidebar user={user} />
+				{children}
+			</main>
+		</Fragment>
+	);
+}
