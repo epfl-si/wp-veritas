0efd41db4ebb4b883bdc8bb01178d4a45538ac6b
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-10 20:43:44 +0200

    [fix] correct component name and translation key for SiteListPage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,14 @@
 'use server';
 
 import { Error } from '@/components/error';
-import { SitesList } from '@/components/pages/sites/list';
+import { SiteList } from '@/components/pages/site/list';
 import { getUserPermissions } from '@/services/auth';
 import { listSites } from '@/services/site';
 import { CircleX } from 'lucide-react';
 import { getTranslations } from 'next-intl/server';
 
-export default async function SitesListPage() {
-	const t = await getTranslations('sites.list');
+export default async function SiteListPage() {
+	const t = await getTranslations('site.list');
 	const { sites, error } = await listSites();
 
 	const permissions = await getUserPermissions();
@@ -25,5 +25,5 @@ export default async function SitesListPage() {
 		return <Error text={t('error.empty')} subText="" Icon={CircleX} color="text-red-500" />;
 	}
 
-	return <SitesList sites={sites} permissions={permissions} />;
+	return <SiteList sites={sites} permissions={permissions} />;
 }
