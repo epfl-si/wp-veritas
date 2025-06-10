948c3dff2594c0f66be4960c0ae5d94591f3edc8
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-10 20:31:30 +0200

    [refactor] standardize error message keys in useZodErrorMessages and getZodErrorMessages functions

--- src/hooks/zod.ts
+++ src/hooks/zod.ts
@@ -1,15 +1,30 @@
 import { useTranslations } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
 
 export const useZodErrorMessages = () => {
 	const t = useTranslations('validation');
 
 	return {
-		required_error: t('required'),
-		invalid_type_error: t('invalid_type'),
-		too_small: (min: number) => t('too_small', { min }),
-		too_big: (max: number) => t('too_big', { max }),
-		invalid_url: t('invalid_url'),
-		invalid_string: t('invalid_string'),
-		invalid_enum: (options: string[]) => t('invalid_enum', { options: options.join(', ') }),
+		required: t('required'),
+		invalid_string: t('invalidString'),
+		invalid_type: t('invalidType'),
+		too_small: (min: number) => t('tooSmall', { min }),
+		too_big: (max: number) => t('tooBig', { max }),
+		invalid_url: t('url'),
+		invalid_enum: (options: string[]) => t('invalidEnum', { options: options.join(', ') }),
+	};
+};
+
+export const getZodErrorMessages = async () => {
+	const t = await getTranslations('validation');
+
+	return {
+		required: t('required'),
+		invalid_string: t('invalidString'),
+		invalid_type: t('invalidType'),
+		too_small: (min: number) => t('tooSmall', { min }),
+		too_big: (max: number) => t('tooBig', { max }),
+		invalid_url: t('url'),
+		invalid_enum: (options: string[]) => t('invalidEnum', { options: options.join(', ') }),
 	};
 };
