105fb8d92a4a50a11e1f61516576a76d190a992e
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-10 20:19:14 +0200

    [add] introduce THEMES constant for theme management and update ThemeType definition

---
+++ src/constants/theme.ts
@@ -0,0 +1,10 @@
+export const THEMES = {
+	LIGHT: {
+		NAME: 'wp-theme-light',
+		LABEL: { fr: 'WP Light', en: 'WP Light' },
+	},
+	WWW: {
+		NAME: 'wp-theme-2018',
+		LABEL: { fr: 'WP 2018', en: 'WP 2018' },
+	},
+};
--- src/types/theme.ts
+++ src/types/theme.ts
@@ -1,22 +1,3 @@
-import { useZodErrorMessages } from '@/hooks/zod';
-import { z } from 'zod';
+import { THEMES } from '@/constants/theme';
 
-export interface ThemeType {
-	id: string;
-	name: string;
-}
-
-export const ThemeSchema = (errorMessages: ReturnType<typeof useZodErrorMessages>) => {
-	return z.object({
-		name: z
-			.string({
-				required_error: errorMessages.required_error,
-				invalid_type_error: errorMessages.invalid_type_error,
-			})
-			.min(1, {
-				message: errorMessages.too_small(1),
-			}),
-	});
-};
-
-export type ThemeFormType = z.infer<ReturnType<typeof ThemeSchema>>;
+export type ThemeType = (typeof THEMES)[keyof typeof THEMES];
