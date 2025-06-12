b0deb3fcbd72277ccdaa5d4c6106c161ff6fd202
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-12 16:21:45 +0200

    [fix] replace img with Image component for better optimization in Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { LanguageSelector } from '@/components/language-selector';
 import { User } from 'next-auth';
 import { UserRound } from 'lucide-react';
+import Image from 'next/image';
 
 export const Header: React.FC<{ user: User }> = ({ user }) => {
 	return (
 		<header className="text-primary-secondary py-2 px-2 sm:py-4 sm:px-6 flex items-center justify-between border-b-2 border-0 select-none">
 			<div className="flex items-center gap-2 sm:gap-4 p-1 sm:p-3">
-				<img src="https://epfl-si.github.io/elements/svg/epfl-logo.svg" alt="LOGO" className="h-4 sm:h-7" />
+				<Image src="https://epfl-si.github.io/elements/svg/epfl-logo.svg" alt="EPFL" width={97} height={28} className="h-4 sm:h-7" />
 				<span className="border-l-2 border-solid sm:h-6 h-4 w-1 border-gray-300"></span>
 				<h1 className="text-base sm:text-2xl font-bold -ml-1 sm:ml-0">WP-Veritas</h1>
 			</div>
