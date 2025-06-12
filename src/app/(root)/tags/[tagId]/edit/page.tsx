a7a78c6f7671ef47fd00eb23012bfcc733aa33d7
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-12 16:20:06 +0200

    [feature] implement TagUpdatePage for editing tags with error handling

--- src/app/(root)/tags/[tagId]/edit/page.tsx
+++ src/app/(root)/tags/[tagId]/edit/page.tsx
@@ -1,7 +1,29 @@
 'use server';
+import { Error } from '@/components/error';
+import { TagUpdate } from '@/components/pages/tags/update';
+import { getTag } from '@/services/tags';
+import { CircleX } from 'lucide-react';
+import { getTranslations } from 'next-intl/server';
 
-import { TagAdd } from '@/components/pages/tags/add';
+export default async function TagUpdatePage({
+	params,
+}: {
+	params: {
+		tagId: string;
+	};
+}) {
+	const t = await getTranslations('tag.update');
+	const { tagId } = await params;
 
-export default async function TagAddPage() {
-	return <TagAdd />;
+	const { tag, error } = await getTag(tagId);
+
+	if (error) {
+		return <Error text={t('error.text')} subText={error.message} Icon={CircleX} color="text-red-500" />;
+	}
+
+	if (!tag) {
+		return <Error text={t('error.empty')} subText={t('error.subText')} Icon={CircleX} color="text-red-500" />;
+	}
+
+	return <TagUpdate tag={tag} />;
 }
