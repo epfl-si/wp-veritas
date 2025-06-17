72a353f8e06f405dd0ae1b833036833a09152e99
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-17 08:11:37 +0200

    [fix] remove informational banner from RootLayout component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -10,7 +10,6 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
 
 	return (
 		<Fragment>
-			<div className="bg-red-500 w-full p-3 text-center text-white font-semibold">This is a rebirth of WP-Veritas. Only operations performed directly on sites will be executed. Tags changes made here will not be reflected in the actual WP-Veritas instance. </div>
 			<Header user={user} />
 			<main className="flex sm:h-[calc(100%-90px)]">
 				<Sidebar user={user} />
