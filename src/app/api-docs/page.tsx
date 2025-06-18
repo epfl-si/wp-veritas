094fd3d6062e73babeb41aed07d3447ee6b532d3
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-18 16:51:33 +0200

    [feature] add API documentation generation with Swagger

---
+++ src/app/api-docs/page.tsx
@@ -0,0 +1,7 @@
+import { getApiDocs } from '@/lib/swagger';
+import ReactSwagger from './react-swagger';
+
+export default async function IndexPage() {
+	const spec = (await getApiDocs()) as Record<string, unknown>;
+	return <ReactSwagger spec={spec} />;
+}
