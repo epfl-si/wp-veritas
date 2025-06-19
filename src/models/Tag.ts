17c8822849a8b4964caba14c6956761de8160bfa
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-19 08:29:16 +0200

    [refactor] enhance Tag model by enforcing type definitions and required fields

--- src/models/Tag.ts
+++ src/models/Tag.ts
@@ -1,20 +1,28 @@
 import mongoose, { Document, ObjectId } from 'mongoose';
+import type { TagCategoryEnumType } from '@/types/tag';
+import { TAG_CATEGORIES_VALUES } from '@/constants/tags';
 
 export interface ITag extends Document {
 	_id: ObjectId;
 	id: string;
-	type: string;
+	type: TagCategoryEnumType;
 	nameFr: string;
 	nameEn: string;
 	urlFr: string;
 	urlEn: string;
-	sites?: string[];
+	sites: string[];
+	createdAt: Date;
+	updatedAt: Date;
 }
 
 const tagSchema = new mongoose.Schema<ITag>(
 	{
 		id: { type: String, required: true, unique: true },
-		type: { type: String, required: true },
+		type: {
+			type: String,
+			required: true,
+			enum: TAG_CATEGORIES_VALUES,
+		},
 		nameFr: { type: String, required: true },
 		nameEn: { type: String, required: true },
 		urlFr: { type: String, required: true },
