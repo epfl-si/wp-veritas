8863aa27aec9d7f28fa13a8e8cc52a9cad339ed3
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-20 15:43:24 +0200

    [fix] Adjust padding in PopoverTrigger for improved layout

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -399,7 +399,7 @@ export default function Form<T extends FieldValues>({ config, className = "" }: 
 					<PopoverTrigger asChild>
 						<div 
 							className={cn(
-								"inline-flex items-center gap-2 whitespace-nowrap rounded-md text-sm transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 px-4 has-[>svg]:px-3 w-full h-auto min-h-10 py-0 justify-between text-left font-normal", 
+								"inline-flex items-center gap-2 whitespace-nowrap rounded-md py-1 text-sm transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 px-4 has-[>svg]:px-3 w-full h-auto min-h-10 justify-between text-left font-normal", 
 								disabled && "cursor-not-allowed opacity-50",
 							)} 
 						>
