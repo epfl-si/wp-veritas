fe58d937f69021a9792166a4d6196509054da940
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-30 14:05:11 +0200

    [fix] Correct environment variable name from REDIS_URL to REDIS_URI

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -5,7 +5,7 @@ class RedisCache {
 	private redis: Redis;
 
 	private constructor() {
-		this.redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379", {
+		this.redis = new Redis(process.env.REDIS_URI || "redis://localhost:6379", {
 			maxRetriesPerRequest: 3,
 			lazyConnect: true,
 		});
