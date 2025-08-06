6ffa7ad67dd221a744f80f15e30b023c5d0a62fc
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-08-06 12:10:15 +0200

    [style] standardize quotes and formatting in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,45 +10,27 @@ const compat = new FlatCompat({
 });
 
 const rules = [
-	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
 	{
-		files: ['**/*.{js,ts,jsx,tsx}'],
+		files: ["**/*.{js,ts,jsx,tsx}"],
 		rules: {
-			indent: [
-				'error',
-				'tab',
-				{
-					SwitchCase: 1,
-					VariableDeclarator: { var: 1, let: 1, const: 1 },
-					outerIIFEBody: 1,
-					FunctionDeclaration: { parameters: 1, body: 1 },
-					FunctionExpression: { parameters: 1, body: 1 },
-					CallExpression: { arguments: 1 },
-					ArrayExpression: 1,
-					ObjectExpression: 1,
-					ImportDeclaration: 1,
-					flatTernaryExpressions: false,
-					ignoredNodes: ['TemplateLiteral *'],
-					ignoreComments: false,
-				},
-			],
-			'no-mixed-spaces-and-tabs': 'error',
-			'no-tabs': 'off',
+			"no-mixed-spaces-and-tabs": "error",
+			"no-tabs": "off",
 			"react-hooks/exhaustive-deps": "off",
-			semi: ['error', 'always'],
-			quotes: ['error', 'double', { avoidEscape: true }],
-			'comma-dangle': ['error', 'always-multiline'],
-			'key-spacing': ['error', { beforeColon: false, afterColon: true }],
-			'object-curly-spacing': ['error', 'always'],
-			'array-bracket-spacing': ['error', 'never'],
-			'space-in-parens': ['error', 'never'],
-			'space-before-blocks': ['error', 'always'],
-			'space-infix-ops': 'error',
-			'no-console': ['error', {
-				allow: ['warn', 'error', "info"]
+			semi: ["error", "always"],
+			quotes: ["error", "double", { avoidEscape: true }],
+			"comma-dangle": ["error", "always-multiline"],
+			"key-spacing": ["error", { beforeColon: false, afterColon: true }],
+			"object-curly-spacing": ["error", "always"],
+			"array-bracket-spacing": ["error", "never"],
+			"space-in-parens": ["error", "never"],
+			"space-before-blocks": ["error", "always"],
+			"space-infix-ops": "error",
+			"no-console": ["error", {
+				allow: ["warn", "error", "info"],
 			}],
-			eqeqeq: ['error', 'always'],
-			curly: ['error', 'multi-line'],
+			eqeqeq: ["error", "always"],
+			curly: ["error", "multi-line"],
 		},
 	},
 ];
