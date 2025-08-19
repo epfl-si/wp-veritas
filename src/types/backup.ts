296d816e14b1c6f4d3f0bafd884df0cb83a17527
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-08-19 11:27:11 +0200

    [feature] add BackupConfig and related interfaces for backup configurations

---
+++ src/types/backup.ts
@@ -0,0 +1,51 @@
+export interface BackupConfig {
+	test: {
+		s3: {
+			bucket: string;
+			endpoint: string;
+			region: string;
+			secretName: string;
+			accessKeyIdSecretKeyRef: string;
+			secretAccessKeySecretKeyRef: string;
+
+		};
+		api: {
+			url: string;
+		};
+	};
+	prod: {
+		s3: {
+			bucket: string;
+			endpoint: string;
+			region: string;
+			secretName: string;
+			accessKeyIdSecretKeyRef: string;
+			secretAccessKeySecretKeyRef: string;
+		};
+		api: {
+			url: string;
+		};
+	};
+}
+
+export type BackupEnvironment = "test" | "prod";
+
+export interface SiteBackupConfig {
+	dbName: string | null;
+	dbRef: string | null;
+	urlSource: string | null;
+}
+
+export interface EnvironmentBackupConfig {
+	s3: {
+		bucket: string;
+		endpoint: string;
+		region: string;
+		secretName: string;
+		accessKeyIdSecretKeyRef: string;
+		secretAccessKeySecretKeyRef: string;
+	};
+	api: {
+		url: string;
+	};
+}
