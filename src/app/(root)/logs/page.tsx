84dc68a37e3bd57170bebd75de17fd329b8ad847
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-09-16 11:07:56 +0200

    [feature] log search completion action in LogListPage

--- src/app/(root)/logs/page.tsx
+++ src/app/(root)/logs/page.tsx
@@ -1,5 +1,11 @@
 import { LogList } from "@/components/pages/log/list";
+import { info } from "@/lib/log";
 
-export default function LogListPage() {
+export default async function LogListPage() {
+	await info("Logs search completed", {
+		type: "log",
+		action: "search",
+	});
+
 	return <LogList />;
 }
