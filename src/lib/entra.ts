f3af86691cd4084f743624068513619516f1004f
dwehs163 <dwesh163@protonmail.com>
2025-12-16 11:43:54 +0100

    [feature] implement Entra application management functions

---
+++ src/lib/entra.ts
@@ -0,0 +1,81 @@
+import { EntraApplication } from "@/types/entra";
+import { isKubernetesSite, SiteType } from "@/types/site";
+
+export async function generateToken(): Promise<string> {
+	const params = new URLSearchParams();
+	params.append("client_id", process.env.AUTH_MICROSOFT_ENTRA_ID ?? "");
+	params.append("client_secret", process.env.AUTH_MICROSOFT_ENTRA_SECRET ?? "");
+	params.append("scope", "api://" + (process.env.AUTH_MICROSOFT_ENTRA_ID ?? "") + "/.default");
+	params.append("grant_type", "client_credentials");
+
+	const response = await fetch(`https://login.microsoftonline.com/${process.env.AUTH_MICROSOFT_ENTRA_TENANT_ID}/oauth2/v2.0/token`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		body: params.toString(),
+	});
+
+	if (!response.ok) {
+		const responseText = await response.text();
+		throw new Error(`Failed to generate token: ${response.status} ${responseText}`);
+	}
+
+	const data = await response.json();
+	return data.access_token;
+}
+
+export async function createApplication(site: SiteType): Promise<EntraApplication> {
+	const token = await generateToken();
+
+	if (!isKubernetesSite(site)) {
+		throw new Error("Site type not supported for Entra application creation");
+	}
+
+	const body = {
+		authorizedUsers: [],
+		config_desc: `Application for ${site.title}`,
+		description: `Application for ${site.title}`,
+		displayName: "WORDPRESS - " + site.title,
+		environmentID: 2,
+		notes: `Entra application for WordPress site ${site.title} (${site.url}) managed by WP-Veritas.`,
+		spa: {
+			redirectUris: [
+				"http://localhost:8080",
+			],
+		},
+		unitID: "13030",
+	};
+
+	const response = await fetch(`${process.env.APP_PORTAL_URL}/app-portal-api/v1/portal/oidc-apps`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": `Bearer ${token}`,
+		},
+		body: JSON.stringify(body),
+	});
+
+	if (!response.ok) {
+		console.error("Create application response:", await response.text());
+		throw new Error(`Failed to create application: ${response.status} ${response.statusText}`);
+	}
+
+	const data = await response.json();
+	return data;
+}
+
+export async function deleteApplication(applicationId: string): Promise<void> {
+	const token = await generateToken();
+
+	const response = await fetch(`https://graph.microsoft.com/v1.0/applications/${applicationId}`, {
+		method: "DELETE",
+		headers: {
+			"Authorization": `Bearer ${token}`,
+		},
+	});
+
+	if (!response.ok) {
+		throw new Error(`Failed to delete application: ${response.status} ${response.statusText}`);
+	}
+}
