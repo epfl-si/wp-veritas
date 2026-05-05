cacc82ff014c24a67408b365e5edf9d76b507e1b
dwesh163 <dwesh163@protonmail.com>
2026-05-05 10:58:05 +0200

    [refactor] adjust padding in Header component for improved layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import { LanguageSelector } from "@/components/language-selector";
 
 export const Header: React.FC<{ user: User }> = ({ user }) => {
 	return (
-		<header className="text-primary-secondary py-2 px-2 sm:py-4 sm:px-6 flex items-center justify-between border-b-2 border-0 select-none">
+		<header className="text-primary-secondary py-2 px-2 sm:py-3 sm:px-5 flex items-center justify-between border-b-2 border-0 select-none">
 			<div className="flex items-center gap-2 sm:gap-4 p-1 sm:p-3">
 				<Image src="https://epfl-si.github.io/elements/svg/epfl-logo.svg" alt="EPFL" width={97} height={28} className="h-4 sm:h-7" />
 				<span className="border-l-2 border-solid sm:h-6 h-4 w-1 border-gray-300"></span>
